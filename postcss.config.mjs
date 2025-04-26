3e59b97b3ece5600a33fa70062a18fd872165a83
dev-roar-lab <dev.roar.lab@gmail.com>
2025-04-26 17:43:01 +0900

    fix: storybookのエラー原因を修正 (#1)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
 const config = {
-  // TODO storybookの自動生成で作成されたがstorybook devでエラーの原因となるためコメントアウト
-  // 後で調査と根本解決をする
-  // plugins: ['@tailwindcss/postcss']
+  plugins: { '@tailwindcss/postcss': {} }
 }
 
 export default config
